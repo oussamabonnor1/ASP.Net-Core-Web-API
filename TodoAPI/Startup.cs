d3f343a6874400bf1b8680d5478195b063ed19b7
oussamabonnor1 <oussamabonnor@gmail.com>
2019-06-04 03:02:28 +0100

    Adding ValuesController and initializing MVC functions

--- TodoAPI/Startup.cs
+++ TodoAPI/Startup.cs
@@ -15,6 +15,7 @@ namespace TodoAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +26,11 @@ namespace TodoAPI
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseMvc();
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("MVC couldn't find shit!");
             });
         }
     }
